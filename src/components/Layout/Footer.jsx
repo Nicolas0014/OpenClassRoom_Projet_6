7f8bb6769ffbfbaa7e2d28d0bc501c7212770be8
Nicolas Charbonnier <charbonnier.nicolas@outlook.fr>
2024-02-08 15:05:34 +0100

    Footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,5 +1,13 @@
+import './Footer.scss';
+import LogoSmallWhite from '../../images/Logo_Small_White.png'
+import { Link } from "react-router-dom";
+
 export default function Footer() {
+
     return (
-        <>Footer</>
+        <footer>
+            <Link to="/"><img className="logo" src={LogoSmallWhite} alt="Logo"/></Link>
+            <p className='copyright'>© 2020 Kasa. All <br/> rights reserved</p>
+        </footer>
     )
 }
